a03cec329c860d11e6bc5f58590086449ae83554
Felipe1114 <63155769+Felipe1114@users.noreply.github.com>
2024-12-12 09:16:03 +0100

    projket abgeeschlossen

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -1,23 +1,31 @@
 import json
-DATAPATH = "animals_data.json"
-
+JSON_DATAPATH = "animals_data.json"
+HTML_DATAPATH = "animals_template.html"
 
 def main():
-  display_animal_informations()
+  update_HTML_data()
 
-def get_data():
-  with open(DATAPATH, "r") as objfile:
+
+def get_json_data():
+  with open(JSON_DATAPATH, "r") as objfile:
     data = json.load(objfile)
   return data
 
 
-def save_data(data):
-  with open(DATAPATH, "w") as objfile:
-    json.dump(data, objfile, indent=4)
+def get_HTML_data():
+  with open(HTML_DATAPATH, "r") as htmlfile:
+    data = htmlfile.read()
+
+    return data
+
+
+def save_HTML_data(data):
+  with open(HTML_DATAPATH, "w") as objfile:
+    objfile.write(data)
 
 
 def get_all_animal_data():
-  data = get_data()
+  data = get_json_data()
   all_animal_informations = []
   for animal in data:
     name = animal["name"]
@@ -36,20 +44,33 @@ def get_all_animal_data():
   return all_animal_informations
 
 
-def display_animal_informations():
+def generate_animal_informations():
   animal_informations = get_all_animal_data()
+  output = ""
 
   for animal in animal_informations:
-
+    output += '<li class="cards__item">'
     name = animal[0]
     diet = animal[1]
     location = animal[2]
 
     if len(animal) == 4:
       type = animal[3]
-      print(f"Name: {name}\nDiet: {diet}\nLocation: {location}\nType: {type}\n")
+      output += f"Name: {name}<br/>\nDiet: {diet}<br/>\nLocation: {location}<br/>\nType: {type}\n</li>\n\n"
     else:
-      print(f"Name: {name}\nDiet: {diet}\nLocation: {location}\n")
+      output += f"Name: {name}<br/>\nDiet: {diet}<br/>\nLocation: {location}\n</li>\n\n"
+
+  return output
+
+
+def update_HTML_data(old_data="__REPLACE_ANIMALS_INFO__"):
+  html = get_HTML_data()
+  animal_info = generate_animal_informations()
+
+  html = html.replace(old_data, animal_info)
+
+  save_HTML_data(html)
+
 
 
 if __name__ == "__main__":
